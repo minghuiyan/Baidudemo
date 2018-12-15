f658b160d1c919d80834354467f2c4ec6e8fe74b
minghuiyan <yanminghuizmm@163.com>
2018-12-15 17:00:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yanmi/baidudemo/MyLocationListener.java
@@ -0,0 +1,11 @@
+package com.example.yanmi.baidudemo;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+
+class MyLocationListener extends BDAbstractLocationListener {
+    @Override
+    public void onReceiveLocation(BDLocation bdLocation) {
+
+    }
+}
